e36a2e3672e9de8ce116876cf828b7d29e7eda67
AlessioCherici <alessio.cherici@hotmail.it>
2023-01-23 15:16:19 +0100

    Creato controller con metodo get pizze

---
+++ Prova1/Controllers/PizzeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyPizzeriaModel.Models;
+using MyPizzeriaModel;
+
+namespace My_pizzeria_Model.Controllers
+    {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzeController : ControllerBase
+        {
+
+        [HttpGet]
+
+        public IActionResult Get()
+            {
+            using PizzaContext db = new PizzaContext();
+            List<Pizza> listaPizze = db.Pizzas.ToList();
+            return Ok(listaPizze);
+            }
+        }
+    }
